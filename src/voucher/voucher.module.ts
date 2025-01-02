4b1d0d8aa4195bfd96d46353a395693cbc1f7abc
mann082 <guptamann108@gmail.com>
2025-01-02 17:57:48 +0530

    fixed getallcampaign

--- src/voucher/voucher.module.ts
+++ src/voucher/voucher.module.ts
@@ -1,4 +1,11 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Voucher, VoucherSchema } from './schemas/voucher.schema';
 
-@Module({})
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Voucher.name, schema: VoucherSchema }]),
+  ],
+  // Add providers and controllers if necessary
+})
 export class VoucherModule {}
