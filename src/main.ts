c30ff3510f79e4f65c6ef2cd9d962f3295753b71
mann082 <guptamann108@gmail.com>
2025-01-02 23:38:15 +0530

    fixed cors policy

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['*', 'http://localhost:3000','https://voucherify-frontend.vercel.app/'],
+    origin: ['*', 'http://localhost:3000','https://voucherify-frontend.vercel.app/','https://vounew.vercel.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     allowedHeaders: 'Content-Type, Accept , Authorization',
     credentials: true, 
